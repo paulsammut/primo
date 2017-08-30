dc88138ffa813baa6329c6457665c98e55e48c14
Paul Sammut <paul@sammut-tech.com>
2017-08-30 10:18:53 -0400

    Got the power sensor to work with the proper resolution

--- src/primo_base/include/power_sensor/power_sensor.ino
+++ src/primo_base/include/power_sensor/power_sensor.ino
@@ -13,22 +13,25 @@ sensor_msgs::BatteryState msg_batt;
 ros::Publisher pub_battState("battery", &msg_batt);
 
 
-void setup() 
+void setup()
 {
     nh.getHardware()->setBaud(115200);
     nh.initNode();
     nh.advertise(pub_battState);
 }
 
-void loop() 
+void loop()
 {
     if(millis() > timePub)
     {
-       
-    msg_batt.voltage = analogRead(PIN_VOLTAGE);
-    msg_batt.current = analogRead(PIN_CURRENT);
-    pub_battState.publish(&msg_batt);
-    nh.spinOnce();
+        // This is from the conversion of the power sensor
+        // 50V/90A = 63.69mV / Volt 36.60mV / Amp
+        // msg_batt.voltage = (1024.0/((float)analogRead(PIN_VOLTAGE)))*5.0;//0.06369;
+        msg_batt.voltage = (((float)analogRead(PIN_VOLTAGE))/1024.0*5.0)/0.06369;
+        msg_batt.current = (((float)analogRead(PIN_CURRENT))/1024.0*5.0)/0.03660;
+        pub_battState.publish(&msg_batt);
+        nh.spinOnce();
+        timePub += 50;
     }
 }
 
