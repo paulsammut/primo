7e7e8a34d17317f527188ead387c034495d66ce5
Paul Sammut <paul.c.sammut@gmail.com>
2017-08-29 21:22:29 -0400

    About to test the power sensor

--- src/primo_base/include/power_sensor/power_sensor.ino
+++ src/primo_base/include/power_sensor/power_sensor.ino
@@ -3,18 +3,32 @@
 
 #define PIN_VOLTAGE A0
 #define PIN_CURRENT A1
+
 ros::NodeHandle nh;
+
+unsigned long timePub = 0;
 
 sensor_msgs::BatteryState msg_batt;
 
 ros::Publisher pub_battState("battery", &msg_batt);
 
 
-void setup() {
-  msg_batt.voltage = 23;
+void setup() 
+{
+    nh.getHardware()->setBaud(115200);
+    nh.initNode();
+    nh.advertise(pub_battState);
 }
 
-void loop() {
-  
+void loop() 
+{
+    if(millis() > timePub)
+    {
+       
+    msg_batt.voltage = analogRead(PIN_VOLTAGE);
+    msg_batt.current = analogRead(PIN_CURRENT);
+    pub_battState.publish(&msg_batt);
+    nh.spinOnce();
+    }
 }
 
