fffe80414277960f137ecafa536075b9af5e3fb6
Paul Sammut <paul@sammut-tech.com>
2017-09-01 15:10:17 -0400

    Fixed the power sensor. It works friggen excellent, and it is accurate!

--- src/primo_base/include/power_sensor/power_sensor.ino
+++ src/primo_base/include/power_sensor/power_sensor.ino
@@ -1,5 +1,6 @@
 #include <ros.h>
 #include <sensor_msgs/BatteryState.h>
+#include <ros/time.h>
 
 #define PIN_VOLTAGE A0
 #define PIN_CURRENT A1
@@ -29,8 +30,14 @@ void loop()
         // msg_batt.voltage = (1024.0/((float)analogRead(PIN_VOLTAGE)))*5.0;//0.06369;
         msg_batt.voltage = (((float)analogRead(PIN_VOLTAGE))/1024.0*5.0)/0.06369;
         msg_batt.current = (((float)analogRead(PIN_CURRENT))/1024.0*5.0)/0.03660;
+
+        // This are my own calibration offsets
+        msg_batt.voltage += -0.48;
+
+        msg_batt.header.stamp = nh.now();
+
         pub_battState.publish(&msg_batt);
         nh.spinOnce();
-        timePub += 50;
+        timePub = 50 + millis();
     }
 }
