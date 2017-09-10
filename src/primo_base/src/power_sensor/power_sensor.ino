5bccee531b0531d5ec4909b44f087790ee3d8240
Paul Sammut <paul@sammut-tech.com>
2017-09-09 20:46:01 -0400

    Adding the power sensor. Curious to why I don't have the power correction..

--- src/primo_base/src/power_sensor/power_sensor.ino
+++ src/primo_base/src/power_sensor/power_sensor.ino
@@ -4,10 +4,17 @@
 
 #define PIN_VOLTAGE A0
 #define PIN_CURRENT A1
+#define PIN_LED_GREEN 2
+#define PIN_LED_RED 3
+#define PIN_SWITCH 4
 
 ros::NodeHandle nh;
 
 unsigned long timePub = 0;
+
+// Interval in milliseconds for publishing the battery data
+int pubInterval = 50;
+float voltLow = 21.5;
 
 sensor_msgs::BatteryState msg_batt;
 
@@ -16,6 +23,19 @@ ros::Publisher pub_battState("battery", &msg_batt);
 
 void setup()
 {
+    // Set the pin modes
+    pinMode(PIN_LED_GREEN, OUTPUT);
+    pinMode(PIN_LED_RED, OUTPUT);
+    pinMode(PIN_SWITCH, OUTPUT);
+    
+    // Set everything low to be nice and safe
+    digitalWrite(PIN_LED_RED, LOW);
+    digitalWrite(PIN_LED_GREEN, LOW);
+    digitalWrite(PIN_SWITCH, LOW);
+
+    // Do an initial protection check
+    protectionCheck();
+
     nh.getHardware()->setBaud(115200);
     nh.initNode();
     nh.advertise(pub_battState);
@@ -25,10 +45,8 @@ void loop()
 {
     if(millis() > timePub)
     {
-        // This is from the conversion of the power sensor
-        // 50V/90A = 63.69mV / Volt 36.60mV / Amp
-        msg_batt.voltage = (((float)analogRead(PIN_VOLTAGE))/1024.0*5.0)/0.06369;
-        msg_batt.current = (((float)analogRead(PIN_CURRENT))/1024.0*5.0)/0.03660;
+        msg_batt.voltage = readVoltage();
+        msg_batt.current = readCurrent();
 
         // This is my own calibration offsets
         msg_batt.voltage += -0.48;
@@ -37,6 +55,44 @@ void loop()
 
         pub_battState.publish(&msg_batt);
         nh.spinOnce();
-        timePub = 50 + millis();
+        timePub = pubInterval + millis();
+
+        protectionCheck();
+    }
+}
+
+float readVoltage(void)
+{
+    // This is from the conversion of the power sensor
+    // Volt 36.60mV / Amp
+    float retVolts = (((float)analogRead(PIN_VOLTAGE))/1024.0*5.0)/0.06369;
+    return(retVolts);
+}
+
+float readCurrent(void)
+{
+    // 50V/90A = 63.69mV 
+    float retCurrent = (((float)analogRead(PIN_CURRENT))/1024.0*5.0)/0.03660;
+    return retCurrent;
+}
+
+bool protectionCheck(void)
+{
+    // System is down
+    if(readVoltage() <= voltLow)
+    {
+        digitalWrite(PIN_SWITCH, LOW);
+        digitalWrite(PIN_LED_RED, HIGH);
+        digitalWrite(PIN_LED_GREEN, LOW);
+        return true;
+    } 
+
+    // System is good
+    else 
+    {
+        digitalWrite(PIN_SWITCH, HIGH);
+        digitalWrite(PIN_LED_RED, LOW);
+        digitalWrite(PIN_LED_GREEN, HIGH);
+        return false;
     }
 }
