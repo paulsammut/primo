66befe9830a600cfe344c5283623a6e1adb1f665
Paul Sammut <paul@sammut-tech.com>
2017-09-11 18:01:32 -0400

    Starting work on the uimu code

---
+++ src/memsense_uimu/include/uimu.h
@@ -0,0 +1,64 @@
+#pragma once
+
+#include "serial/serial.h"
+#include <iostream>
+
+#define STOOTH_BAUD 9600
+
+/**
+ * \class SabertoothSimple
+ *
+ * \brief Simple driver for the sabertooth 2x** modules using Plain Text Serial. This
+ * node uses the wjwood serial ros library.
+ *
+ * \author Paul Sammut
+ *
+ * Contact: paul@sammut-tech.com
+ *
+ */
+class SabertoothSimple
+{
+    public:
+        /**
+         * @brief Sets the power output for either motor 1 or motor 2
+         *
+         * @param motorNum The number of the motor, either 1 or 2
+         * @param powerVal Power value which goes from -2047 to 2047
+         *
+         * @return Returns 1 if successful, 0 if fail.
+         */
+        int setM(int motorNum, int powerVal);
+        /**
+         * @brief Gets the current power setting for either motor 1 or motor 2
+         *
+         * @param motorNum The number of the motor, either 1 or 2
+         * @param powerVal Pointer to the power value which goes from -2047 to 2047
+         *
+         * @return Returns 1 if successful, 0 if fail.
+         */
+        int getM(int motorNum, int &powerVal);
+
+        /**
+         * @brief Default constructor that may or may not do anything
+         */
+        SabertoothSimple(void);
+
+        /**
+         * @brief Destructor that may or may not do anything
+         */
+        ~SabertoothSimple(void);
+
+        /**
+         * @brief Opens the port and connects to the sabertooth. Automatically looks
+         * for the sabertooth driver :)
+         *
+         * @return 1 if success, 0 if failed
+         */
+        int connect(void);
+    private:
+        /**
+         * @brief This is the serial object that handles connection with the
+         * sabertooth module
+         */
+        serial::Serial stoothSerPort;
+};
---
+++ src/memsense_uimu/src/uimu.cpp
@@ -0,0 +1,97 @@
+#include "serial/serial.h"
+#include "sabertooth_simple.h"
+#include <ros/ros.h>
+#include <string>
+#include <iostream>
+#include <cstdio>
+#include <sstream>
+
+using std::vector;
+using std::cout;
+
+int SabertoothSimple::setM(int motorNum, int powerVal)
+{
+    // First make sure serial port is open
+    if(stoothSerPort.isOpen()) {
+        // Formulate our command string
+        std::string cmdString;
+        if(motorNum==1)
+            cmdString = "M1:";
+        else if(motorNum==2)
+            cmdString = "M2:";
+        else
+            return 0;
+
+        std::ostringstream cmdNum;
+        cmdNum << powerVal;
+        cmdString += cmdNum.str();
+        cmdString += "\r\n";
+        stoothSerPort.write(cmdString);
+        return 1;
+    } else
+        return 0;
+}
+
+
+int SabertoothSimple::getM(int motorNum, int &powerVal)
+{
+    powerVal = 69;
+    return 1;
+}
+
+int SabertoothSimple::connect(void)
+{
+    // These are used to iterate through the serial port and look for the Sabertooth
+    // module. Taken from serial examples.
+    vector<serial::PortInfo> devices_found = serial::list_ports();
+    vector<serial::PortInfo>::iterator iter = devices_found.begin();
+
+    while(iter != devices_found.end())
+    {
+        serial::PortInfo device = *iter++;
+
+        // Look for the sabertooth module.
+        if(strstr(device.description.c_str(), "Sabertooth") != NULL)
+        {
+            ROS_INFO( "Found: (%s, %s, %s)\n", device.port.c_str(), device.description.c_str(),
+                      device.hardware_id.c_str() );
+
+            stoothSerPort.setPort(device.port.c_str());
+            stoothSerPort.setBaudrate(STOOTH_BAUD);
+
+            // This gave me loads of trouble. You have to set a sensible timeout or
+            // else the port won't work.
+            serial::Timeout timeout = serial::Timeout::simpleTimeout(1000);
+            stoothSerPort.setTimeout(timeout);
+
+            // Open the serial port
+            stoothSerPort.open();
+            if(stoothSerPort.isOpen())
+            {
+                ROS_INFO("Sabertooth port opened! %d baud, %s port",
+                         stoothSerPort.getBaudrate(), stoothSerPort.getPort().c_str()) ;
+            }
+            else
+            {
+                ROS_ERROR("Port was not able to opened");
+                return 0;
+            }
+
+            std::string daters = "M1:0\r\nM2:0\r\n";
+            size_t writtenBees = stoothSerPort.write(daters);
+            // ROS_INFO("Wrote %lu bytes", writtenBees);
+        }
+    }
+    return 1;
+}
+
+SabertoothSimple::SabertoothSimple(void)
+{
+}
+
+SabertoothSimple::~SabertoothSimple(void)
+{
+    setM(1,0);
+    setM(2,0);
+    stoothSerPort.close();
+}
