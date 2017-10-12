42e1fa27c7dda3d9c40cec16846e3fa74f205ff4
Paul Sammut <paul@sammut-tech.com>
2017-10-12 09:02:54 -0400

    Doing a commit on the robot before doing some work on the laptop

--- src/differential-drive/src/primo_diff_tf.cpp
+++ src/differential-drive/src/primo_diff_tf.cpp
@@ -1,5 +1,6 @@
 #include <ros/ros.h>
 #include <tf2_ros/transform_broadcaster.h>
+#include <string>
 
 int main(int argc, char** argv)
 {
@@ -8,8 +9,18 @@ int main(int argc, char** argv)
 
     tf2_ros::TransformBroadcaster odom_braodcaster;
 
-
     ros::Rate loop_rate(10);
+   
+    ROS_INFO("Primo Diff TF running!");
+
+    double rate;
+    double tickst_meter;
+    double base_width;
+
+    std::string base_frame_id, odom_frame_id, wheel_frame_id;
+
+    long encoder_min, encoder_max, encoder_low_wrap, encoder_high_wrap;
+
 
     while(ros::ok())
     {
