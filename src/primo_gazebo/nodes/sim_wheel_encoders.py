19e88e3adf0e02e9436a2961f74efa8174f303ce
Paul Sammut <paul@sammut-tech.com>
2017-10-13 12:11:03 -0400

    got both wheel angles

--- src/primo_gazebo/nodes/sim_wheel_encoders.py
+++ src/primo_gazebo/nodes/sim_wheel_encoders.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python
+
+# This node is to simulate the encoder data coming from the motors. I use the wheel
+# angle data coming from gazebo and convert it to encoder values. The reason I built
+# this simulator is so I can test the coordinate transform wheel odom.
 import rospy
 
 import math
@@ -18,18 +22,30 @@ if __name__ == '__main__':
     rate = rospy.Rate(30.0)
 
     while not rospy.is_shutdown():
+
+        # Get the transform for the wheels
         try:
-            trans  = tfBuffer.lookup_transform('chassis_link', 'left_wheel',
+            trans_left_wheel  = tfBuffer.lookup_transform('chassis_link', 'left_wheel',
+                    rospy.Time())
+            trans_right_wheel  = tfBuffer.lookup_transform('chassis_link', 'right_wheel',
                     rospy.Time())
         except (tf2_ros.LookupException, tf2_ros.ConnectivityException,
                 tf2_ros.ExtrapolationException):
             rate.sleep()
             continue
 
-        euler = tf.transformations.euler_from_quaternion([trans.transform.rotation.x, 
-            trans.transform.rotation.y, 
-            trans.transform.rotation.z, 
-            trans.transform.rotation.w]) 
+        euler_left_wheel = tf.transformations.euler_from_quaternion(
+                [trans_left_wheel.transform.rotation.x, 
+                trans_left_wheel.transform.rotation.y, 
+                trans_left_wheel.transform.rotation.z, 
+                trans_left_wheel.transform.rotation.w]) 
         
-        rospy.loginfo("The transform is:%f" %euler[1])
+        euler_right_wheel = tf.transformations.euler_from_quaternion(
+                [trans_right_wheel.transform.rotation.x, 
+                trans_right_wheel.transform.rotation.y, 
+                trans_right_wheel.transform.rotation.z, 
+                trans_right_wheel.transform.rotation.w]) 
+        
+        rospy.loginfo("LEFT: %f \t RIGHT: %f" %(euler_left_wheel[1],
+            euler_right_wheel[1]))
         rate.sleep()
