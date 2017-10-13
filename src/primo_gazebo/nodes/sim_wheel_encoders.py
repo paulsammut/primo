f618592b85b0765798e60ff82180eaf1614679a5
Paul Sammut <paul@sammut-tech.com>
2017-10-13 10:19:43 -0400

    There it is, I got the wheel angle extracted from the tf that's being posted by the diff controller.

--- src/primo_gazebo/nodes/sim_wheel_encoders.py
+++ src/primo_gazebo/nodes/sim_wheel_encoders.py
@@ -3,6 +3,7 @@ import rospy
 
 import math
 import tf2_ros
+import tf
 import geometry_msgs.msg
 
 if __name__ == '__main__':
@@ -12,9 +13,23 @@ if __name__ == '__main__':
     listener = tf2_ros.TransformListener(tfBuffer)
 
     # Calculate the wheel conversion
-    ticks_meter = rospy.get_param('ticks_meter', 69000))
+    ticks_meter = rospy.get_param('ticks_meter', 69000)
 
-    rate = rospy.Rate(50.0)
+    rate = rospy.Rate(30.0)
 
-    while note rospy.is_shutdown():
+    while not rospy.is_shutdown():
+        try:
+            trans  = tfBuffer.lookup_transform('chassis_link', 'left_wheel',
+                    rospy.Time())
+        except (tf2_ros.LookupException, tf2_ros.ConnectivityException,
+                tf2_ros.ExtrapolationException):
+            rate.sleep()
+            continue
+
+        euler = tf.transformations.euler_from_quaternion([trans.transform.rotation.x, 
+            trans.transform.rotation.y, 
+            trans.transform.rotation.z, 
+            trans.transform.rotation.w]) 
+        
+        rospy.loginfo("The transform is:%f" %euler[1])
         rate.sleep()
