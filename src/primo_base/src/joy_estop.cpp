012290c4e513a67696d4a9088138d7b9e3232ba3
Paul Sammut <paul@sammut-tech.com>
2017-10-22 13:34:27 -0400

    Estop finished!

--- src/primo_base/src/joy_estop.cpp
+++ src/primo_base/src/joy_estop.cpp
@@ -1,12 +1,43 @@
 #include <ros/ros.h>
 #include "sabertooth_simple/SabertoothEstop.h"
+#include <sensor_msgs/Joy.h>
+
+ros::ServiceClient client;
+sabertooth_simple::SabertoothEstop srv;
+
+
+void joyCb(const sensor_msgs::Joy::ConstPtr& msg)
+{
+    bool estop_button = msg->buttons[3];
+    
+    if(estop_button)
+    {
+        srv.request.estop = true;
+        if(client.call(srv))
+            ROS_INFO("ESTOP succesfully TURNED ON"); 
+        else
+            ROS_INFO("ESTOP TURN ON FAILED"); 
+    }
+    else if(msg->buttons[2])
+    {
+        srv.request.estop = false;
+        if(client.call(srv))
+            ROS_INFO("ESTOP succesfully cleared"); 
+        else
+            ROS_INFO("ESTOP CLEAR FAILED"); 
+    }
+}
 
 int main(int argc, char **argv)
 {
-    ros::NodeHandle n;
-    ros::ServiceClient client = n.serviceClient<sabertooth_simple::SabertoothEstop>("motor_estop");
+    ros::init(argc, argv, "joy_estop");
 
-    sabertooth_simple::SabertoothEstop srv;
+    ros::NodeHandle n;
+    client = n.serviceClient<sabertooth_simple::SabertoothEstop>("motor_estop");
+
+    ros::Subscriber joySub = n.subscribe("/joy",1000, joyCb);
+
+    ros::spin();
 
     return 1;
 }
