f5bdc2f859ec3515d5157a25652b5a2b3747e4ac
Paul Sammut <paul@sammut-tech.com>
2017-10-27 08:45:42 -0400

    Added sounds to the estop

--- src/primo_base/src/joy_estop.cpp
+++ src/primo_base/src/joy_estop.cpp
@@ -1,23 +1,34 @@
 #include <ros/ros.h>
 #include "sabertooth_simple/SabertoothEstop.h"
 #include <sensor_msgs/Joy.h>
+// This is so we can make the robot make sound
+#include "unistd.h"
+#include "sound_play/sound_play.h"
 
+// Sound client as a pointer so that it doesn't get initialized till later
+sound_play::SoundClient *sc;
 ros::ServiceClient client;
-sabertooth_simple::SabertoothEstop srv;
 bool curState = false;
 
 
 void joyCb(const sensor_msgs::Joy::ConstPtr& msg)
 {
+    sabertooth_simple::SabertoothEstop srv;
     bool estop_button = msg->buttons[3];
     
     if(estop_button)
     {
         srv.request.estop = true;
         if(client.call(srv))
+        {
+            sc->say("Eeee STOP ACTIVATED. I aint goooin noowheereess");
             ROS_INFO("ESTOP succesfully TURNED ON"); 
+        }
         else
-            ROS_INFO("ESTOP TURN ON FAILED"); 
+        {
+            sc->say("FAKKKKKK");
+            ROS_ERROR("ESTOP FAILED"); 
+        }
         curState = true;
     }
     // Emergency Stop clear button
@@ -26,11 +37,15 @@ void joyCb(const sensor_msgs::Joy::ConstPtr& msg)
         srv.request.estop = false;
         if(client.call(srv))
         {
+            sc->say("PRIMO TORQUED AND READY TO FAKK");
             ROS_INFO("ESTOP succesfully cleared"); 
             curState = false;
         }
         else
-            ROS_INFO("ESTOP CLEAR FAILED"); 
+        {
+            sc->say("SOMETHING FUCKED UP");
+            ROS_ERROR("ESTOP CLEAR FAILED"); 
+        }
     }
 }
 
@@ -39,11 +54,14 @@ int main(int argc, char **argv)
     ros::init(argc, argv, "joy_estop");
 
     ros::NodeHandle n;
+    sc = new sound_play::SoundClient;
     client = n.serviceClient<sabertooth_simple::SabertoothEstop>("motor_estop");
 
     ros::Subscriber joySub = n.subscribe("/joy",1000, joyCb);
 
     ros::spin();
+
+    delete sc;
 
     return 1;
 }
