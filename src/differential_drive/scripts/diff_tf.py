fd493e369b4b40bed387235e097d2e9b1e00f97e
Paul Sammut <paul@sammut-tech.com>
2017-11-18 20:40:05 -0500

    Grrrr these settings are in too many files!! Need to make a localized config file for all this shit

--- src/differential_drive/scripts/diff_tf.py
+++ src/differential_drive/scripts/diff_tf.py
@@ -35,8 +35,8 @@ class DiffTf:
         
         #### parameters #######
         self.rate = rospy.get_param('~rate',10.0)  # the rate at which to publish the transform
-        self.ticks_meter = float(rospy.get_param('ticks_meter', 59000))  # The number of wheel encoder ticks per meter of travel
-        self.base_width = float(rospy.get_param('base_width', 0.84)) # The wheel base width in meters
+        self.ticks_meter = float(rospy.get_param('ticks_meter', 68500))  # The number of wheel encoder ticks per meter of travel
+        self.base_width = float(rospy.get_param('base_width', 0.65)) # The wheel base width in meters
         
         self.base_frame_id = rospy.get_param('~base_frame_id','base_link') # the name of the base frame of the robot
         self.odom_frame_id = rospy.get_param('~odom_frame_id', 'odom') # the name of the odometry reference frame
