919cacc95817e229c7c1813f72b2489e0b9aa10b
paulsammut <paul.c.sammut@gmail.com>
2017-12-16 09:34:03 -0500

    Changed the joy_estop node to joy_manager because I added more functionality to it and it stopped being just an estop tool. Did some renaming and updated the CMakeLists file

--- src/primo_base/src/joy_manager.cpp
+++ src/primo_base/src/joy_manager.cpp
@@ -8,50 +8,67 @@
 
 // Sound client as a pointer so that it doesn't get initialized till later
 sound_play::SoundClient *sc;
+// Service client that receive the estop requests
 ros::ServiceClient client;
+// Waypoing follower publisher so we can send a message to start the waypoints.
 ros::Publisher waypoint_pub;
-bool curState = false;
 
+bool estopState = false;
 
 void joyCb(const sensor_msgs::Joy::ConstPtr& msg)
 {
     sabertooth_simple::SabertoothEstop srv;
-    bool estop_button = (msg->buttons[3] || msg->buttons[10] || msg->buttons[9]);
+
+    // Buttons
+    bool btn_a = msg->buttons[0];
+    bool btn_b = msg->buttons[1];
+    bool btn_x = msg->buttons[2];
+    bool btn_y = msg->buttons[3];
+    bool btn_l_paddle = msg->buttons[4];
+    bool btn_r_paddle = msg->buttons[5];
+    bool btn_back = msg->buttons[6];
+    bool btn_start = msg->buttons[7];
+    bool btn_left_stick  = msg->buttons[9];
+    bool btn_right_stick = msg->buttons[10];
+
+    bool estop_button = (btn_left_stick || btn_right_stick);
     
+    // Emergency Stop Activate
     if(estop_button)
     {
+        // set our service object to be true so we engage the estop
         srv.request.estop = true;
         if(client.call(srv))
         {
-            // sc->say("Eeee STOP ACTIVATED. I aint goin NOWHERE");
-            sc->say("ESPOT Activated");
+            sc->say("Emergency stop activated");
             ROS_INFO("ESTOP succesfully TURNED ON"); 
         }
         else
         {
-            sc->say("FAKKKKKK");
-            ROS_ERROR("ESTOP FAILED"); 
+            sc->say("Emergency stop failed to activate");
+            ROS_ERROR("ESTOP failed to activate"); 
         }
         curState = true;
     }
     // Emergency Stop clear button
-    else if(msg->buttons[7] && curState)
+    else if(btn_start && curState)
     {
+        // set our service object to be false so we can clear the estop
         srv.request.estop = false;
         if(client.call(srv))
         {
-            sc->say("PRIMO TORQUED AND READY TO FUCK SHIT UP");
+            sc->say("Primo torqued and ready to fuck shit uup");
             ROS_INFO("ESTOP succesfully cleared"); 
             curState = false;
         }
         else
         {
-            sc->say("SOMETHING FUCKED UP");
+            sc->say("Emergency stop failed to clear");
             ROS_ERROR("ESTOP CLEAR FAILED"); 
         }
     }
     // Waypoint follow button
-    else if(msg->buttons[2])
+    else if(btn_x)
     {
         std_msgs::Empty msg;
         sc->say("Starting mission. Let's do this!");
@@ -63,7 +80,6 @@ void joyCb(const sensor_msgs::Joy::ConstPtr& msg)
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "joy_estop");
-
     ros::NodeHandle n;
     sc = new sound_play::SoundClient;
     client = n.serviceClient<sabertooth_simple::SabertoothEstop>("motor_estop");
