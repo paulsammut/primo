8c3a867d35a48a6eca9686bc103cf9b30946799f
paulsammut <paul.c.sammut@gmail.com>
2017-12-29 21:31:20 -0500

    Yay! It compiles and run and it even merges! Unfortunately i didn't think about frames. I have to convert one cloud from one frame to another

--- src/primo_stereo/src/pcl_merge.cpp
+++ src/primo_stereo/src/pcl_merge.cpp
@@ -3,13 +3,14 @@
 #include "pcl_conversions/pcl_conversions.h"
 #include <string>
 
-static sensor_msgs::PointCloud2 channel0_buffer, channel1_buffer;
+static sensor_msgs::PointCloud2 channel0_buffer, channel1_buffer, output;
 
-static pcl::PCLPointCloud2 pclPCacumulada;
+static pcl::PCLPointCloud2 channel0_temp, channel1_temp, output_temp;
 
 
 void channel0_Cb(sensor_msgs::PointCloud2 PCmsg)
 {
+    channel0_buffer = PCmsg;
     // pcl::PCLPointCloud2 pclmsg;
     // pcl_conversions::toPCL(PCmsg,pclmsg);
     // pcl::concatenatePointCloud (pclPCacumulada, pclmsg, pclPCacumulada);
@@ -18,6 +19,7 @@ void channel0_Cb(sensor_msgs::PointCloud2 PCmsg)
 
 void channel1_Cb(sensor_msgs::PointCloud2 PCmsg)
 {
+    channel1_buffer = PCmsg;
 }
 
 int main (int argc, char **argv)
@@ -32,8 +34,8 @@ int main (int argc, char **argv)
     ros::NodeHandle np; 
 
     // Get the parameters
-    ros::param::param<std::string>("channel0", channel0_topic, "channel0");
-    ros::param::param<std::string>("channel1", channel1_topic, "channel1");
+    ros::param::param<std::string>("channel0", channel0_topic, "/stereo0/cbx/output");
+    ros::param::param<std::string>("channel1", channel1_topic, "/stereo1/cbx/output");
     ros::param::param<double>("pub_rate", pub_rate, 10);
 
     // Make the subscribers
@@ -47,7 +49,20 @@ int main (int argc, char **argv)
     ros::spinOnce();
     while(ros::ok())
     {       
-        // publicador.publish(PCacumulada);
+        // convert to PCL from sensor_msgs:PCL
+        pcl_conversions::toPCL(channel0_buffer, channel0_temp);
+        pcl_conversions::toPCL(channel1_buffer, channel1_temp);
+
+        // Do the merge to the output variable
+        pcl::concatenatePointCloud(channel0_temp, channel1_temp, output_temp);
+
+        // convert back to sensor_msgs:PCL
+        pcl_conversions::fromPCL(output_temp, output);
+
+        // publish our merged point cloud!
+        pcl_pub.publish(output);
+
+        // Do the ROS
         ros::spinOnce();
         loop_rate.sleep();
     }
