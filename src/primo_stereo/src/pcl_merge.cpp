6300ce9699a762b6be8a0ca085a2e48184f08277
paulsammut <paul.c.sammut@gmail.com>
2017-12-29 19:37:49 -0500

    Got it to compile

--- src/primo_stereo/src/pcl_merge.cpp
+++ src/primo_stereo/src/pcl_merge.cpp
@@ -2,31 +2,31 @@
 #include "sensor_msgs/PointCloud2.h"
 #include "pcl_conversions/pcl_conversions.h"
 
-static sensor_msgs::PointCloud2 PCacumulada;  
+// static sensor_msgs::PointCloud2 PCacumulada;  
 
-static pcl::PCLPointCloud2 pclPCacumulada;
+// static pcl::PCLPointCloud2 pclPCacumulada;
 
 
-void chatterCallback( sensor_msgs::PointCloud2 PCmsg)
-{
-    pcl::PCLPointCloud2 pclmsg;
-    pcl_conversions::toPCL(PCmsg,pclmsg);
-    pcl::concatenatePointCloud (pclPCacumulada, pclmsg, pclPCacumulada);
-    pcl_conversions::fromPCL(pclPCacumulada, PCacumulada);  
-}
+// void chatterCallback( sensor_msgs::PointCloud2 PCmsg)
+// {
+    // pcl::PCLPointCloud2 pclmsg;
+    // pcl_conversions::toPCL(PCmsg,pclmsg);
+    // pcl::concatenatePointCloud (pclPCacumulada, pclmsg, pclPCacumulada);
+    // pcl_conversions::fromPCL(pclPCacumulada, PCacumulada);  
+// }
 
 int main (int argc, char **argv)
 {
     ros::init(argc,argv,"nodeProcesador"); 
     ros::NodeHandle ns; 
     ros::NodeHandle np; 
-    ros::Subscriber subscriptor = ns.subscribe("velodyne_points",10, chatterCallback);
-    ros::Publisher publicador= ns.advertise<sensor_msgs::PointCloud2>("velodyne_points2",10);
+    // ros::Subscriber subscriptor = ns.subscribe("velodyne_points",10, chatterCallback);
+    // // ros::Publisher publicador= ns.advertise<sensor_msgs::PointCloud2>("velodyne_points2",10);
     ros::Rate loop_rate(10); 
     ros::spinOnce();
     while(ros::ok())
     {       
-        publicador.publish(PCacumulada);
+        // publicador.publish(PCacumulada);
         ros::spinOnce();
         loop_rate.sleep();
     }
