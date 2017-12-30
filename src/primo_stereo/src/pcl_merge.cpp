c136609fa4a69b55d8a4b241b89b92c998f39e0b
paulsammut <paul.c.sammut@gmail.com>
2017-12-29 20:01:55 -0500

    Cleaned up the ros package by folling the pcl tutorial here http://wiki.ros.org/pcl/Tutorials

--- src/primo_stereo/src/pcl_merge.cpp
+++ src/primo_stereo/src/pcl_merge.cpp
@@ -1,32 +1,40 @@
 #include "ros/ros.h"
 #include "sensor_msgs/PointCloud2.h"
 #include "pcl_conversions/pcl_conversions.h"
+#include <string>
 
-static sensor_msgs::PointCloud2 PCacumulada;  
+static sensor_msgs::PointCloud2 channel0_buffer, channel1_buffer;
 
 static pcl::PCLPointCloud2 pclPCacumulada;
 
 
 void chatterCallback( sensor_msgs::PointCloud2 PCmsg)
 {
-    pcl::PCLPointCloud2 pclmsg;
-    pcl_conversions::toPCL(PCmsg,pclmsg);
-    pcl::concatenatePointCloud (pclPCacumulada, pclmsg, pclPCacumulada);
-    pcl_conversions::fromPCL(pclPCacumulada, PCacumulada);  
+    // pcl::PCLPointCloud2 pclmsg;
+    // pcl_conversions::toPCL(PCmsg,pclmsg);
+    // pcl::concatenatePointCloud (pclPCacumulada, pclmsg, pclPCacumulada);
+    // pcl_conversions::fromPCL(pclPCacumulada, PCacumulada);  
 }
 
 int main (int argc, char **argv)
 {
-    ros::init(argc,argv,"nodeProcesador"); 
+    // topic names to subscribe to
+    std::string channel0, channel1;
+
+    ros::init(argc,argv,"pcl_merge"); 
     ros::NodeHandle ns; 
     ros::NodeHandle np; 
-    ros::Subscriber subscriptor = ns.subscribe("/stereo0/cbx/output",10, chatterCallback);
+
+    // Get the parameters
+    // ros::param
+
+    // ros::Subscriber subscriptor = ns.subscribe("/stereo0/cbx/output",10, chatterCallback);
     ros::Publisher publicador= ns.advertise<sensor_msgs::PointCloud2>("velodyne_points2",10);
     ros::Rate loop_rate(10); 
     ros::spinOnce();
     while(ros::ok())
     {       
-        publicador.publish(PCacumulada);
+        // publicador.publish(PCacumulada);
         ros::spinOnce();
         loop_rate.sleep();
     }
