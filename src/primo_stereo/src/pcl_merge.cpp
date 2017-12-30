abf24be9f4e8ebe1b1ad823ed11aa998e69c7439
paulsammut <paul.c.sammut@gmail.com>
2017-12-29 21:12:58 -0500

    Fleshing out the pcl_merge node

--- src/primo_stereo/src/pcl_merge.cpp
+++ src/primo_stereo/src/pcl_merge.cpp
@@ -8,7 +8,7 @@ static sensor_msgs::PointCloud2 channel0_buffer, channel1_buffer;
 static pcl::PCLPointCloud2 pclPCacumulada;
 
 
-void chatterCallback( sensor_msgs::PointCloud2 PCmsg)
+void channel0_Cb(sensor_msgs::PointCloud2 PCmsg)
 {
     // pcl::PCLPointCloud2 pclmsg;
     // pcl_conversions::toPCL(PCmsg,pclmsg);
@@ -16,21 +16,34 @@ void chatterCallback( sensor_msgs::PointCloud2 PCmsg)
     // pcl_conversions::fromPCL(pclPCacumulada, PCacumulada);  
 }
 
+void channel1_Cb(sensor_msgs::PointCloud2 PCmsg)
+{
+}
+
 int main (int argc, char **argv)
 {
     // topic names to subscribe to
-    std::string channel0, channel1;
+    std::string channel0_topic, channel1_topic;
+
+    double pub_rate;
 
     ros::init(argc,argv,"pcl_merge"); 
     ros::NodeHandle ns; 
     ros::NodeHandle np; 
 
     // Get the parameters
-    // ros::param
+    ros::param::param<std::string>("channel0", channel0_topic, "channel0");
+    ros::param::param<std::string>("channel1", channel1_topic, "channel1");
+    ros::param::param<double>("pub_rate", pub_rate, 10);
 
-    // ros::Subscriber subscriptor = ns.subscribe("/stereo0/cbx/output",10, chatterCallback);
-    ros::Publisher publicador= ns.advertise<sensor_msgs::PointCloud2>("velodyne_points2",10);
-    ros::Rate loop_rate(10); 
+    // Make the subscribers
+    ros::Subscriber channel0_sub = ns.subscribe(channel0_topic,10, channel0_Cb);
+    ros::Subscriber channel1_sub = ns.subscribe(channel1_topic,10, channel1_Cb);
+
+    ros::Publisher pcl_pub = ns.advertise<sensor_msgs::PointCloud2>("output",10);
+
+    ros::Rate loop_rate(pub_rate); 
+
     ros::spinOnce();
     while(ros::ok())
     {       
