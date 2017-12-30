07375b7ef888cc5fa4756d2d2d0f07a0adc5747e
paulsammut <paul.c.sammut@gmail.com>
2017-12-29 22:42:59 -0500

    Wow it finally fucking works

--- src/primo_stereo/src/pcl_merge.cpp
+++ src/primo_stereo/src/pcl_merge.cpp
@@ -1,20 +1,25 @@
-#include "ros/ros.h"
-#include "sensor_msgs/PointCloud2.h"
-#include "pcl_conversions/pcl_conversions.h"
+#include <ros/ros.h>
+#include <tf/transform_listener.h>
+#include <tf/tf.h>
+#include <sensor_msgs/PointCloud2.h>
+#include <pcl_conversions/pcl_conversions.h>
+#include <pcl_ros/transforms.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
 #include <string>
+#include "tf/transform_datatypes.h"
+#include "Eigen/Core"
+#include "Eigen/Geometry"
 
 static sensor_msgs::PointCloud2 channel0_buffer, channel1_buffer, output;
 
 static pcl::PCLPointCloud2 channel0_temp, channel1_temp, output_temp;
 
 
+
 void channel0_Cb(sensor_msgs::PointCloud2 PCmsg)
 {
     channel0_buffer = PCmsg;
-    // pcl::PCLPointCloud2 pclmsg;
-    // pcl_conversions::toPCL(PCmsg,pclmsg);
-    // pcl::concatenatePointCloud (pclPCacumulada, pclmsg, pclPCacumulada);
-    // pcl_conversions::fromPCL(pclPCacumulada, PCacumulada);  
 }
 
 void channel1_Cb(sensor_msgs::PointCloud2 PCmsg)
@@ -25,18 +30,28 @@ void channel1_Cb(sensor_msgs::PointCloud2 PCmsg)
 int main (int argc, char **argv)
 {
     // topic names to subscribe to
-    std::string channel0_topic, channel1_topic;
+    std::string channel0_topic, 
+                channel1_topic,
+                channel0_frame,
+                channel1_frame;
 
     double pub_rate;
 
+    // ROS init stuff
     ros::init(argc,argv,"pcl_merge"); 
     ros::NodeHandle ns; 
-    ros::NodeHandle np; 
+    
+    // create our transform listener
+    tf::TransformListener listener;
+    tf::StampedTransform transform;
 
     // Get the parameters
     ros::param::param<std::string>("channel0", channel0_topic, "/stereo0/cbx/output");
     ros::param::param<std::string>("channel1", channel1_topic, "/stereo1/cbx/output");
+    ros::param::param<std::string>("channel0_frame", channel0_frame, "stereo0_link");
+    ros::param::param<std::string>("channel1_frame", channel1_frame, "stereo1_link");
     ros::param::param<double>("pub_rate", pub_rate, 10);
+
 
     // Make the subscribers
     ros::Subscriber channel0_sub = ns.subscribe(channel0_topic,10, channel0_Cb);
@@ -45,10 +60,24 @@ int main (int argc, char **argv)
     ros::Publisher pcl_pub = ns.advertise<sensor_msgs::PointCloud2>("output",10);
 
     ros::Rate loop_rate(pub_rate); 
+    
+    // Get our transform
+    try{
+        listener.lookupTransform("/stereo0_link", "/stereo1_link",  
+                ros::Time(0), transform);
+    }
+    catch (tf::TransformException ex){
+        ROS_ERROR("%s",ex.what());
+        ros::Duration(1.0).sleep();
+    }
 
     ros::spinOnce();
     while(ros::ok())
     {       
+        
+        //convert channel1 to be in the channel0 frame
+        pcl_ros::transformPointCloud("/stereo0_link", channel1_buffer, channel1_buffer, listener);
+
         // convert to PCL from sensor_msgs:PCL
         pcl_conversions::toPCL(channel0_buffer, channel0_temp);
         pcl_conversions::toPCL(channel1_buffer, channel1_temp);
