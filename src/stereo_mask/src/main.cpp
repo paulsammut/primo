015ff10c34ce7615ddced3f230b17c322f45ef80
paulsammut <paul.c.sammut@gmail.com>
2018-01-15 12:40:01 -0500

    Trying to clean stuff up and in the process I'm breaking things.

--- src/stereo_mask/src/main.cpp
+++ src/stereo_mask/src/main.cpp
@@ -8,40 +8,11 @@
 image_transport::Publisher pubLeft;
 image_transport::Publisher pubRight;
 
+/**
+ * @name Callback for the left image. Applies the polygon overlay
+ * @{ */
+/**  @} */
 void imageLeftCallback(const sensor_msgs::ImageConstPtr& msg)
-{
-    try
-    {
-        cv::Mat image = cv_bridge::toCvShare(msg, "mono8")->image;
-
-        // Ok cool, now we have an image, and now we need to draw on it!
-        // Make the polygon
-
-        cv::Point pts[1][4];
-        pts[0][0] = cv::Point(   0, 479 );
-        pts[0][1] = cv::Point( 400, 222 );
-        pts[0][2] = cv::Point( 500, 300 );
-        pts[0][3] = cv::Point( 624, 479 );
-
-        // Set up the points for the filly poly function
-        const cv::Point* ppt[1] = { pts[0] };
-        int npt[] = {4};
-
-        // boost::timer::cpu_timer timer1;
-        cv::fillPoly(image, ppt, npt, 1, cv::Scalar(0, 0, 0), cv::LINE_8);
-        // printf(timer1.format().c_str());
-
-        sensor_msgs::ImagePtr msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", image).toImageMsg();
-
-        pubLeft.publish(msg);
-    }
-    catch (cv_bridge::Exception& e)
-    {
-        ROS_ERROR("Could not convert from '%s' to 'bgr8'.", msg->encoding.c_str());
-    }
-}
-
-void imageRightCallback(const sensor_msgs::ImageConstPtr& msg)
 {
     try
     {
@@ -67,11 +38,52 @@ void imageRightCallback(const sensor_msgs::ImageConstPtr& msg)
         sensor_msgs::ImagePtr msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", image).toImageMsg();
 
         // Publish the image
+        pubLeft.publish(msg);
+    }
+    catch (cv_bridge::Exception& e)
+    {
+        ROS_ERROR("Could not convert from '%s' to 'mono8'.", msg->encoding.c_str());
+    }
+}
+
+/**
+ * @name Callback for the right image. Applies the polygon overlay
+ * @{ */
+/**  @} */
+void imageRightCallback(const sensor_msgs::ImageConstPtr& msg)
+{
+    try
+    {
+        cv::Mat image = cv_bridge::toCvShare(msg, "mono8")->image;
+        // Ok cool, now we have an image, and now we need to draw on it!
+        // Make the polygon
+
+        cv::Point pts[1][4];
+        pts[0][0] = cv::Point(   0, 479 );
+        pts[0][1] = cv::Point( 400, 222 );
+        pts[0][2] = cv::Point( 500, 300 );
+        pts[0][3] = cv::Point( 624, 479 );
+
+        // Set up the points for the filly poly function
+        const cv::Point* ppt[1] = { pts[0] };
+        int npt[] = {4};
+
+        // boost::timer::cpu_timer timer1;
+        cv::fillPoly(image, ppt, npt, 1, cv::Scalar(0, 0, 0), cv::LINE_8);
+        // printf(timer1.format().c_str());
+
+        // Convert to ros image
+        // keep the header 
+        std_msgs::Header hdr;
+        hdr.stamp = msg->header.stamp;
+        sensor_msgs::ImagePtr msg = cv_bridge::CvImage(std_msgs::Header(), "mono8", image).toImageMsg();
+
+        // Publish the image
         pubRight.publish(msg);
     }
     catch (cv_bridge::Exception& e)
     {
-        ROS_ERROR("Could not convert from '%s' to 'bgr8'.", msg->encoding.c_str());
+        ROS_ERROR("Could not convert from '%s' to 'mono8'.", msg->encoding.c_str());
     }
 }
 
@@ -83,12 +95,21 @@ int main(int argc, char **argv)
     ros::init(argc, argv, "stereo_mask");
     ros::NodeHandle nh;
 
-    image_transport::ImageTransport it(nh);
-    image_transport::Subscriber subLeft = it.subscribe("/stereo0/left/image_raw", 1, imageLeftCallback);
-    image_transport::Subscriber subRight = it.subscribe("/stereo0/right/image_raw", 1, imageRightCallback);
+    // Get the parameters
+    ros::param::param<std::string>("~left_image_topic",   topicLeft,  "left/image_raw" );
+    ros::param::param<std::string>("~right_image_topic",  topicRight, "right/image_raw");
 
-    pubLeft = it.advertise("/stereo0/left/image_raw_mask", 1);
-    pubRight = it.advertise("/stereo0/right/image_raw_mask", 1);
+    // Debug info
+    ROS_INFO("param left_image_topic with value:%s",    topicLeft.c_str());
+    ROS_INFO("param right_image_topic with value:%s",   topicRight.c_str());
+
+    // Handle the image transport subscribers and publishers
+    image_transport::ImageTransport it(nh);
+    image_transport::Subscriber subLeft     =   it.subscribe(topicLeft,   1, imageLeftCallback);
+    image_transport::Subscriber subRight    =   it.subscribe(topicRight,  1, imageRightCallback);
+
+    pubLeft = it.advertise("left/image_raw_mask", 1);
+    pubRight = it.advertise("right/image_raw_mask", 1);
 
     ros::spin();
 }
