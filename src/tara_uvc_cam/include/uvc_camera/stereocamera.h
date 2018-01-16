2f2121c572e15a70e3ee4de7fd7656c8c4531c05
paulsammut <paul.c.sammut@gmail.com>
2018-01-16 16:13:33 -0500

    Minor formatting

--- src/tara_uvc_cam/include/uvc_camera/stereocamera.h
+++ src/tara_uvc_cam/include/uvc_camera/stereocamera.h
@@ -12,31 +12,31 @@
 
 namespace uvc_camera {
 
-class StereoCamera {
-  public:
-    StereoCamera(ros::NodeHandle comm_nh, ros::NodeHandle param_nh);
-    void onInit();
-    void sendInfo(ros::Time time);
-    void feedImages();
-    ~StereoCamera();
+    class StereoCamera {
+      public:
+        StereoCamera(ros::NodeHandle comm_nh, ros::NodeHandle param_nh);
+        void onInit();
+        void sendInfo(ros::Time time);
+        void feedImages();
+        ~StereoCamera();
 
-  private:
-    ros::NodeHandle node, pnode;
-    image_transport::ImageTransport it;
-    bool ok;
+      private:
+        ros::NodeHandle node, pnode;
+        image_transport::ImageTransport it;
+        bool ok;
 
-    uvc_cam::Cam *cam_left, *cam_right;
-    int width, height, fps, skip_frames, frames_to_skip;
-    std::string left_device, right_device, frame;
-    bool rotate_left, rotate_right;
+        uvc_cam::Cam *cam_left, *cam_right;
+        int width, height, fps, skip_frames, frames_to_skip;
+        std::string left_device, right_device, frame;
+        bool rotate_left, rotate_right;
 
-    camera_info_manager::CameraInfoManager left_info_mgr, right_info_mgr;
+        camera_info_manager::CameraInfoManager left_info_mgr, right_info_mgr;
 
-    image_transport::Publisher left_pub, right_pub;
-    ros::Publisher left_info_pub, right_info_pub;
+        image_transport::Publisher left_pub, right_pub;
+        ros::Publisher left_info_pub, right_info_pub;
 
-    boost::thread image_thread;
-};
+        boost::thread image_thread;
+    };
 
 };
 
