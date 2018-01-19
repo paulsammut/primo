f0aa3eefd73c784017e7dd2d67b3a4d19f26f392
paulsammut <paul.c.sammut@gmail.com>
2018-01-19 15:41:03 -0500

    Trying to change the class to a nodelet class

--- src/stereo_mask/include/stereo_mask/stereo_mask.h
+++ src/stereo_mask/include/stereo_mask/stereo_mask.h
@@ -1,22 +1,17 @@
-#include <ros/ros.h>
+#include "ros/ros.h"
+#include "nodelet/nodelet.h"
 #include <image_transport/image_transport.h>
 #include <opencv2/highgui/highgui.hpp>
 #include <cv_bridge/cv_bridge.h>
 #include <opencv2/core.hpp>
 #include <opencv2/imgproc.hpp>
+#include <pluginlib/class_list_macros.h>
 
 namespace stereo_mask
 {
-class StereoMask
+class StereoMask : public nodelet::Nodelet
 {
 public:
-    /**
-     * @brief constructor that takes in the nodehandle.
-     *
-     * @param nh
-     */
-    StereoMask(ros::NodeHandle _nh);
-
     /**
      * @brief Callback for receiving the image
      *
@@ -32,11 +27,15 @@ public:
     void imageRightCb(const sensor_msgs::ImageConstPtr& msg);
 
 private:
+    virtual void onInit();
+
     ros::NodeHandle nh;
+    ros::NodeHandle private_nh;
     image_transport::ImageTransport it;
     image_transport::Publisher pubLeft;
     image_transport::Publisher pubRight;
     image_transport::Subscriber subLeft;
     image_transport::Subscriber subRight;
+
 };
 }
