fd84b3a922a7196729dab371b5724a623494a7a7
paulsammut <paul.c.sammut@gmail.com>
2018-01-27 15:26:25 -0500

    Adding the fancy script that disables the xinput of the camera

---
+++ src/primo_stereo/scripts/stereo2_fix.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+searchStr="e-con's 1MP Monochrome Camera"
+
+# search for the cameras
+if xinput list | grep -q "$searchStr"
+then
+    echo "Found the offending e-con camera(s)";
+else
+    echo "No e-con cameras found!";
+    exit 0
+fi
+
+
+xinput list | grep "$searchStr" | while read -r line ; do
+    var=${line#*=}
+    id=$(awk '{print $1}' <<< $var)
+
+    echo "Disabling $var"
+    echo xinput --disable $id
+
+
+done
