2c76532353ded539a0d52d84cd67b283203ad03a
Paul Sammut <paul@sammut-tech.com>
2018-02-02 21:54:13 -0500

    Getting a recording script for mapping:

--- src/primo_base/record/mapping.sh
+++ src/primo_base/record/mapping.sh
@@ -3,11 +3,10 @@
 # Mapping recording script
 
 rosbag record \
-    /stereo2/left/image_raw \
-    /stereo2/right/image_raw \
-    /stereo2/left/camera_info \
-    /stereo2/right/camera_info \
     /odometry/filtered \
-    /tf_static \
-    /tf
-
+    /stereo2/left/image_rect_color \
+    /stereo2/depth \
+    /stereo2/left/image_rect \
+    /stereo2/left/camera_info
+    /tf \
+    /tf_static
