a771d422ac8e134e0810040f35634a3d7391287e
Paul Sammut <paul@sammut-tech.com>
2018-02-17 12:54:46 -0500

    added stuff for rtab

--- src/primo_record/sensor_suite.sh
+++ src/primo_record/sensor_suite.sh
@@ -2,19 +2,19 @@
 
 rosbag record   \
                 /stereo0/left/image_raw \
+                /stereo0/left/image_rect \
                 /stereo0/left/camera_info \
-                /stereo0/right/image_raw \
-                /stereo0/right/camera_info \
                 /stereo1/left/image_raw \
                 /stereo1/left/camera_info \
-                /stereo1/right/image_raw \
-                /stereo1/right/camera_info \
+                /stereo1/left/image_rect \
                 /stereo2/left/image_raw \
                 /stereo2/left/camera_info \
-                /stereo2/right/image_raw \
-                /stereo2/right/camera_info \
-                /stereo3/left/image_raw_color
-                /stereo3/left/camera_info
+                /stereo2/left/image_rect \
+                /stereo2/left/image_rect_color \
+                /stereo2/depth \
+                /stereo3/left/image_raw_color \
+                /stereo3/left/image_rect_color \
+                /stereo3/left/camera_info \
                 /stereo0/vxl/output \
                 /stereo1/vxl/output \
                 /stereo2/vxl/output \
@@ -27,3 +27,5 @@ rosbag record   \
                 /tf \
                 /tf_static 
                 /odometry/filtered \
+                /goal \
+                /goal_node \
