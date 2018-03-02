c2a998faaf3686f695d6bdbeb76cbe785adf442d
paulsammut <paul.c.sammut@gmail.com>
2018-03-02 15:34:02 -0500

    Yay test script works

--- src/primo_description/urdf/align_test_script.sh
+++ src/primo_description/urdf/align_test_script.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 rosrun xacro xacro --inorder primo_real.xacro > test.urdf \
-    align_s0:=false \
+    align_s0:=true \
     align_s1:=true \
-    align_s2:=false \
+    align_s2:=true \
     align_s3:=true \
-    align_c0:=false \
+    align_c0:=true \
     && urdf_to_graphiz test.urdf && evince primo.pdf && \
     rm test.urdf primo.pdf primo.gv
