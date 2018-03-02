d0ee94a871454129a2aa0977204ff5be7dbe2f8a
paulsammut <paul.c.sammut@gmail.com>
2018-03-02 15:32:06 -0500

    Adding test script

--- src/primo_description/urdf/align_test_script.sh
+++ src/primo_description/urdf/align_test_script.sh
@@ -5,4 +5,5 @@ rosrun xacro xacro --inorder primo_real.xacro > test.urdf \
     align_s2:=false \
     align_s3:=true \
     align_c0:=false \
-    && urdf_to_graphiz test.urdf && evince primo.pdf
+    && urdf_to_graphiz test.urdf && evince primo.pdf && \
+    rm test.urdf primo.pdf primo.gv
