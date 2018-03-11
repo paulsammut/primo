12c9130a1a95cbb69618f89650e5564cac09f1df
paulsammut <paul.c.sammut@gmail.com>
2018-03-11 13:42:57 -0400

    Adding the color0 fix script

--- src/primo_record/scripts/camera_info.py
+++ src/primo_record/scripts/camera_info.py
@@ -8,29 +8,52 @@ import rospy
 import rosbag
 import sys
 
-topicName  = "/color0/camera_info"
-newLink    = "color0_link"
-newHeight  = 480
-newWidth   = 864
+topicCamInfo    = "/color0/camera_info"
+topicImgRaw     = "/color0/image_raw"
+newLink         = "color0_link"
+newHeight       = 480
+newWidth        = 864
 
 
 if __name__ == '__main__':
+
+    # Make sure we have the right number of arguments
     if len(sys.argv) == 3:
-      with rosbag.Bag(sys.argv[2],'w') as outbag:
-          for topic, msg, t in rosbag.Bag(sys.argv[1]).read_messages():
-              # If it is the color0
-              if topic == topicName:
-                  print(msg)
-                  # Lets rewrite the camere_info message
-                  msg.header.frame_id = newLink
-                  msg.height = newHeight
-                  msg.width = newWidth
-                  outbag.write(topic,msg,t)
-              else:
-                  outbag.write(topic, msg, msg.header.stamp if msg._has_header else t)
+        print("Opening bag ..")
+        with rosbag.Bag(sys.argv[2],'w') as outbag:
+
+            print("Iterating through messages..")
+            # Iterate through the messages
+            for topic, msg, t in rosbag.Bag(sys.argv[1]).read_messages():
+
+
+                # camera_info
+                if topic == topicCamInfo:
+                    # Lets rewrite the camere_info message
+                    msg.header.frame_id = newLink
+                    msg.height = newHeight
+                    msg.width = newWidth
+
+                    msg.distortion_model = "plumb_bob"
+                    msg.D = [0.057168, -0.105575, 0.005278, -0.00021, 0.0]
+                    msg.K = [667.992222, 0.0, 428.0704, 0.0, 670.288747,
+                             254.611786, 0.0, 0.0, 1.0]
+                    msg.R = [1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0]
+                    msg.P = [672.2948, 0.0, 427.771502, 0.0, 0.0, 675.529114,
+                             256.500945, 0.0, 0.0, 0.0, 1.0, 0.0]
+
+                    outbag.write(topic, msg, t)
+
+                elif topic == topicImgRaw:
+                    msg.header.frame_id = newLink
+                    outbag.write(topic, msg, t)
+                    
+                else:
+                    outbag.write(topic, msg, t)
+
+    # Complain about not having the right amount of arguments
     else:
         print "usage: %s input output" % sys.argv[0]
-
 
 # header:
 #     seq: 1132
@@ -55,4 +78,3 @@ if __name__ == '__main__':
 #     height: 0
 #     width: 0
 #     do_rectify: False
-
